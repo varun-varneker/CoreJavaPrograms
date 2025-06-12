ad811a7411aab7857d56a3d3986503125ac9ac80
varun-varneker <varunvarneker06@gmail.com>
2025-06-12 11:54:21 +0530

    today i worked on addition program and learnt how to commit to github

--- C2TC_javaPrograms/src/day1/SampleJava.java
+++ C2TC_javaPrograms/src/day1/SampleJava.java
@@ -3,8 +3,14 @@ package day1;
 public class SampleJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a,b,c;
+		a = Integer.parseInt(args[0]);
+		b = Integer.parseInt(args[1]);
+		c = a+b;
+		System.out.println(c);
+        
+        
+        
 	}
 
 }
