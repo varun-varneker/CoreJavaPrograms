d8f24c4cbcb0424a6165b3e3ee7b4ce0395ce3bd
mosina003 <smosina003@gmail.com>
2025-06-14 22:51:19 +0530

    encapsulation methods

---
+++ C2TC_javaPrograms/src/demo/StudentDemo.java
@@ -0,0 +1,29 @@
+package demo;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		Student stt = new Student();
+		stt.setId(69);
+		stt.setName("Varun");
+		stt.setAge(21);
+		System.out.println(stt);
+		
+		Student stt1 = new Student();
+		stt1.setId(79);
+		stt1.setName("Mosina");
+		stt1.setAge(22);
+		System.out.println(stt1);
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
