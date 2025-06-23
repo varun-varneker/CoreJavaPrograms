6304cdf473defcc02a2d18ac828dbd379158ad55
varun-varneker <varunvarneker02@gmail.com>
2025-06-23 13:56:49 +0530

    My Day Five Java Programs

---
+++ C2TC_javaPrograms/src/day5/multiLevelInheritance/Car.java
@@ -0,0 +1,13 @@
+package day5.multiLevelInheritance;
+
+public class Car {
+	public Car() {
+		System.out.println("Class Car");
+	}
+
+	public void vehicleType() {
+		System.out.println("Vehicle Type: Car");
+	}
+
+}
+	
